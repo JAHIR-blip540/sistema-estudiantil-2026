396be7530c8fa129d7a9bfa420e0ac4aa80f0ef2
JAHIR-blip540 <antigravity200826@gmail.com>
2026-08-19 07:48:05 -0600

    Delete sistema_estudiantil directory

--- sistema_estudiantil/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Derechos de autor: Jahir Alexander Zuniga Enamorado
-   Duodecimo Grado en Informatica Año 2026 */
-session_start();
-session_destroy();
-header('Location: login.php');
-exit();
-?>
